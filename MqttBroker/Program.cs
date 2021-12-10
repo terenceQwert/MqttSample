85fcafb2dd44779cae64de71c54e785a5eb15025
s8854086 <s8854086@gmail.com>
2021-12-10 18:17:51 +0800

    import first version code note: to support Serilog class, and write information to console, you can not follow original instrution(using app.config or web.config) that is from Serilog githhub. due to this sample was created by general console application. please use nuget package to install Serilog.Sinks.Console.

---
+++ MqttBroker/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Packets;
+using MQTTnet.Protocol;
+using System.Text;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Server;
+using Serilog;
+namespace MqttBroker
+{
+    class Program
+    {
+        static MqttClient mqttClient = null;
+        static IMqttServer mqttServer = null;
+        static IMqttClientOptions options = null;
+        static bool runState = false;
+        static bool running = false;
+        static string ServerUrl = "127.0.0.1";
+        static int port = 8080;
+        static string passowrd = "111111";
+        static string UserId = "admin";
+        static string Topic = "PortalPlayer(TWN) Co, Ltd";
+        static bool Retained = false;
+        static int QualityIOfServiceLevel = 0;
+        public static void Start()
+        {
+            try
+            {
+                runState = true;
+                Thread thread = new Thread(Work);
+                thread.IsBackground = true;
+                thread.Start();
+            } catch (Exception ex)
+            {
+                Console.WriteLine("There is a problem @client side " + ex.ToString());
+            }
+        }
+
+        public static void OnNewConnection( MqttConnectionValidatorContext context)
+        {
+
+            Log.Logger.Information(
+                "New connection: ClientId = {clientId}, Endpoint = {endpoint}",
+                context.ClientId,
+                context.Endpoint);
+        }
+        static int MessageCounter = 0;
+
+        public static void OnNewMessage( MqttApplicationMessageInterceptorContext context)
+        {
+            var payload = context.ApplicationMessage?.Payload == null ? null : context.ApplicationMessage?.Payload;
+            MessageCounter++;
+            Log.Logger.Information(
+                "MessageId: {MessasgeCounter} - TimeStamp: {TimeStamp} -- Message: ClientId = {clientid}, Topic={topic}, Payload={payload}, Qos = {qos}, Rtain-Flag={retainFlag}",
+                MessageCounter,
+                DateTime.Now,
+                context.ClientId,
+                context.ApplicationMessage?.Topic,
+                payload,
+                context.ApplicationMessage?.QualityOfServiceLevel,
+                context.ApplicationMessage?.Retain
+                );
+        }
+        private static void Work()
+        {
+            running = true;
+            Console.WriteLine("Work >> Begin");
+            try
+            {
+                var factory = new MqttFactory();
+                options = new MqttClientOptionsBuilder()
+                    .WithTcpServer(ServerUrl, port)
+                    .WithCredentials(UserId, passowrd)
+                    .WithClientId("XMan")
+                    .Build();
+                MqttServerOptionsBuilder _opts = new MqttServerOptionsBuilder()
+                    .WithDefaultEndpoint()
+                    .WithDefaultEndpointPort(8080)
+                    .WithConnectionValidator(OnNewConnection)
+                    .WithApplicationMessageInterceptor(OnNewMessage);
+                mqttServer = factory.CreateMqttServer();
+                mqttServer.StartAsync(_opts.Build()).GetAwaiter().GetResult();
+                Console.ReadLine();
+
+#if false
+                mqttClient = factory.CreateMqttClient() as MqttClient;
+                options = new MqttClientOptionsBuilder()
+                    .WithTcpServer(ServerUrl, port)
+                    .WithCredentials(UserId, passowrd)
+                    .WithClientId("XMan")
+                    .Build();
+                mqttClient.ConnectAsync(options);
+
+                mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(new Func<MqttClientConnectedEventArgs, Task>(Connected));
+                mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(new Func<MqttClientDisconnectedEventArgs, Task>(Disconnected));
+                mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(new Action<MqttApplicationMessageReceivedEventArgs>(MqttApplicationMessageReceived));
+                while (runState)
+                {
+
+                    Thread.Sleep(100);
+
+                }
+#endif
+            }
+            catch ( Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("Work >> End");
+            running = false;
+            runState = false;
+        }
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            Console.WriteLine("Hello World!");
+            Work();
+        }
+    }
+}
---
+++ MqttClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using Newtonsoft.Json;
+using Serilog;
+namespace MqttClient
+{
+    class Program
+    {
+        public static void OnConnected(MqttClientConnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully connected.");
+        }
+
+        public static void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
+        {
+            Log.Logger.Warning("Couldn't connect to broker.");
+        }
+
+        public static void OnDisconnected(MqttClientDisconnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully disconnected.");
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int port = 8080;
+            
+            // Creates a new client
+            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+                                                    .WithClientId("Dev.To")
+                                                    .WithTcpServer("localhost", port);
+
+            // Create client options objects
+            ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
+                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+                                    .WithClientOptions(builder.Build())
+                                    .Build();
+
+            // Creates the client object
+            IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+            // Set up handlers
+            _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+            _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+            _mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
+
+            // Starts a connection with the Broker
+             _mqttClient.StartAsync(options).GetAwaiter().GetResult();
+
+            // Send a new message to the broker every second
+            while (true)
+            {
+                string json = JsonConvert.SerializeObject(new { message = "Heyo :)", sent = DateTimeOffset.UtcNow });
+                _mqttClient.PublishAsync("dev.to/topic/json", json);
+
+                Task.Delay(1000).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
